85df37d4612b3c666ddefff4e7de46e51d9ded92
Danilo <realdanilo@users.noreply.github.com>
2021-03-12 18:11:24 -0700

    fix styles/color, add link to unique movie id page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import {useState} from "react"
 import {useRouter} from "next/router"
-import axios from "axios"
 import searchAPI from "../utils/searchAPI"
 
 export default function Home({data}) {
@@ -17,7 +17,7 @@ export default function Home({data}) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>{`Searching ${router.query.search}` || "Movie app"}</title>
+        <title>{router.query.search ? `${router.query.search} Movies` : "Movie App - Danilo"}</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Find movies online"/>
         <meta name="keywords" content={`NEXTJS, javascript, ${router.query.search}`} />
@@ -30,26 +30,29 @@ export default function Home({data}) {
         <form onSubmit={(e)=> handleSubmit(e)} className={styles.form}>
             <label>Search: </label>
             <input type="text" onChange={(e)=> setSearch(e.target.value)} value={search}/>
-            <button type="submit">Find</button>
+            <button type="submit">&#128269;</button>
         </form>
         <div className={styles.movieFrame}>
           {data.results.length ===0 && (
             <p className={styles.noResults}>No Results! OMG</p>
           )}
           {data && data.results.map((movie,i) =>(
-            <section key={i} className={styles.movieInfo}>
-              <div className={styles.movieInfoImg}>
-                {movie.poster_path ? (
-                  <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.original_title}/>
-                ): (
-                  <img src="/images/no-cover.png" alt={movie.original_title}/>
-                )}
-              </div>
-              <div className={styles.movieInfoTxt}>
-                <h3>{movie.original_title}</h3>
-                <p>{movie.overview.substring(0,80)}...</p>
-              </div>
-            </section>
+            movie.poster_path && (
+              <Link href={`/movie?id=${movie.id}`}>
+              <a>
+                <section key={i} className={styles.movieInfo}>
+                  <div className={styles.movieInfoImg}>
+                    <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.original_title}/>
+                  </div>
+                  <div className={styles.movieInfoTxt}>
+                    <h3>{movie.original_title}</h3>
+                    <p>{movie.overview.substring(0,80)}...</p>
+                  </div>
+              </section>
+            </a>
+            </Link>
+            )
+           
           ))}
         
        </div>
@@ -64,7 +67,7 @@ export default function Home({data}) {
 export async function getServerSideProps(context){
 //  console.log(context.query)
   let data = await searchAPI(context.query.search)
-
+  
   return{
     props:{
       data,
