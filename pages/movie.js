a9c4f99b119bc3f7728332962f049a62fe015a87
Danilo <realdanilo@users.noreply.github.com>
2021-03-14 10:25:50 -0700

    set front page of movie info, working on extra info such as casting

--- pages/movie.js
+++ pages/movie.js
@@ -2,14 +2,55 @@ import React from 'react'
 import getMovieInfo from "../utils/getMovieInfo"
 import styles from "../styles/Movie.module.css"
 
-const movie = ({ routeId,data}) => {
-    console.log(routeId)
+const timeConvert = (min) => {
+  if(min ===0 ) return "0"
+  let hours  = min / 60 
+  let rhours = Math.floor(hours)
+  let minutes = (hours - rhours) * 60
+  let rminutes = Math.round(minutes)
+  return  `${rhours}h: ${rminutes}min`
+}
+
+const movie = ({ main: data, similar, cast}) => {
+    let dataTime = timeConvert(data.runtime)
+    console.log(data)
     
     return (
         <div className={styles.main} >
-            <div style={{backgroundImage:`url(http://image.tmdb.org/t/p/w500/${data.poster_path});`}} className={styles.backgroundImg}>
+            <div style={{backgroundImage:`url(http://image.tmdb.org/t/p/w500/${data.poster_path})`}} className={styles.backgroundImg}>
                 <div className={styles.backgroundCover}/>
-                {/* <img className={styles.backgroundImg} src={`http://image.tmdb.org/t/p/w500/${data.poster_path}`} alt={data.original_title}/> */}
+                <div className={styles.info}>
+                    <section className={styles.pictureFormat}>
+                      <img src={`http://image.tmdb.org/t/p/w300/${data.poster_path}`} alt={data.title}/>
+                    </section>
+                    <section className={styles.infoText}>
+                      <h3 className={styles.title}>{data.title}</h3>
+                      <span className={styles.releaseDate}>{data.release_date && data.release_date.slice(0,4)}</span>
+                      <span className={styles.time}>{dataTime}</span>
+                      <span className={styles.vote}>{data.vote_average}</span>
+                      <ul className={styles.genres}>
+                        {data.genres.map((genre, id)=><li key={id}>{genre.name}</li>)}
+                      </ul>
+                      <h6 className={styles.tagline}>{data.tagline}</h6>
+                      <h2 className={styles.overview}>Overview</h2>
+                      <p className={styles.overviewText}>{data.overview.slice(0,800)}</p>
+                    </section>
+                </div>
+            </div>
+            <div className={styles.extra}>
+              <section>
+                casting crew first 10
+              </section>
+              <section>
+                <h5>Status</h5>
+                <p>{data.status}</p>
+                <h5>Original Language</h5>
+                <p>{data.original_language.toUpperCase()}</p>
+                <h5>Budget</h5>
+                <p>${data.budget}</p>
+                <h5>Revenue</h5>
+                <p>${data.revenue}</p>
+              </section>
             </div>
         </div>
     )
@@ -18,13 +59,13 @@ const movie = ({ routeId,data}) => {
 export default movie
 
 export async function getServerSideProps(context){
-     let {data} = await getMovieInfo(parseInt(context.query.id))
+     let {main,similar, cast} = await getMovieInfo(parseInt(context.query.id))
       
       return{
         props:{
-          data,
-          routeId:context.query.id
-    
+          main,
+          similar,
+          cast
         }
       }
     }
--- utils/getMovieInfo.js
+++ utils/getMovieInfo.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
 export default async function getMovieInfo (id){
-    // let video = await axios.get(`https://api.themoviedb.org/3/movie/${id}/?api_key=${process.env.movie_api}`)
-    let video = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.movie_api}&language=en-US`)
-    return video
+    let {data: main} = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.movie_api}&language=en-US`)
+    let {data: similar} = await axios.get(`https://api.themoviedb.org/3/movie/${id}/similar?api_key=${process.env.movie_api}&language=en-US&page=1`)
+    let {data: cast} = await axios.get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=${process.env.movie_api}&language=en-US`)
+  
+    return {main, similar,cast}
 }
